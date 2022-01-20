8881490a249780846885b36555413c43b17dcdc0
Ezugwu Joel <ezugwuugwu@gmail.com>
2022-01-20 04:40:22 +0100

    Commit Exercise chapter two

--- src/ChapterTwo/DiameterAndAreaOfCircle.java
+++ src/ChapterTwo/DiameterAndAreaOfCircle.java
@@ -12,16 +12,16 @@ public class DiameterAndAreaOfCircle {
         radius = input.nextInt();
     }
 
-    public  double diameter_Of_Circle(){
+    public  double get_Diameter_Of_Circle(){
 
      return (2 * radius);
     }
 
-    public double circumference_Of_circle(){
+    public double get_Circumference_Of_circle(){
         return 2 * Math.PI *radius;
     }
 
-    public double area_Of_Circle(){
+    public double get_Area_Of_Circle(){
        return Math.PI * Math.pow(radius,2);
     }
 
@@ -29,9 +29,9 @@ public class DiameterAndAreaOfCircle {
 
         DiameterAndAreaOfCircle circleCalc = new DiameterAndAreaOfCircle();
        circleCalc.input_Radius();
-        System.out.printf("%s%8s%12s ", "Diamter", "Circum", "Area");
+        System.out.printf("%s%8s%12s ", "Diameter", "Circum", "Area");
         System.out.println();
-        System.out.printf("%s%10.2f %14.2f", circleCalc.diameter_Of_Circle(), circleCalc.circumference_Of_circle(),circleCalc.area_Of_Circle());
+        System.out.printf("%s%10.2f %14.2f",circleCalc.get_Diameter_Of_Circle(), circleCalc.get_Circumference_Of_circle(), circleCalc.get_Area_Of_Circle());
 
     }
 }
--- src/ChapterTwo/NegativePositiveAndZeroValues.java
+++ src/ChapterTwo/NegativePositiveAndZeroValues.java
@@ -3,28 +3,35 @@ package ChapterTwo;
 import java.util.Scanner;
 
 public class NegativePositiveAndZeroValues {
-    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int positive_Counter =0;
-        int negative_Counter =0;
-        int zero_Counter =0;
+        int positive_Value_Counter =0;
+        int negative_Value_Counter =0;
+        int zero_Value_Counter =0;
 
-
-        for (int i =1; i <=5; i++){
-            System.out.println("Enter five integers");
-            int numbers = input.nextInt();
-            if (numbers >0){
-                positive_Counter = positive_Counter +1;
-            }
-            if (numbers < 0){
-                negative_Counter = negative_Counter +1;
-            }
-            if (numbers ==0){
-                zero_Counter = zero_Counter + 1;
-            }
+public void input_Integers(){
+    for (int i =1; i <=5; i++){
+        System.out.println("Enter five integers");
+        int numbers = input.nextInt();
+        if (numbers >0){
+            positive_Value_Counter = positive_Value_Counter +1;
         }
-        System.out.println("Number of positive input: "+positive_Counter);
-        System.out.println("Number of negative input: "+negative_Counter);
-        System.out.println("Number of zero input: "+zero_Counter);
+        if (numbers < 0){
+            negative_Value_Counter = negative_Value_Counter +1;
+        }
+        if (numbers ==0){
+            zero_Value_Counter = zero_Value_Counter + 1;
+        }
     }
+}
+
+
+    public static void main(String[] args) {
+    NegativePositiveAndZeroValues values = new NegativePositiveAndZeroValues();
+    values.input_Integers();
+        System.out.println("Number of positive input: "+values.positive_Value_Counter);
+        System.out.println("Number of negative input: "+values.negative_Value_Counter);
+        System.out.println("Number of zero input: "+values.zero_Value_Counter);
+
+    }
+
 }
