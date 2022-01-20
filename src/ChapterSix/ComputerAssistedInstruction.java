c464b3b97ae2828109ba9166f8421bb8c0be1c89
Ezugwu Joel <ezugwuugwu@gmail.com>
2022-01-20 09:28:28 +0100

    Commit Exercise chapter two

--- src/ChapterSix/ComputerAssistedInstruction.java
+++ src/ChapterSix/ComputerAssistedInstruction.java
@@ -1,7 +1,6 @@
 package ChapterSix;
 
 import java.security.SecureRandom;
-import java.util.Random;
 import java.util.Scanner;
 
 public class ComputerAssistedInstruction {
@@ -15,22 +14,24 @@ public class ComputerAssistedInstruction {
         System.out.println("How many number of questions do you wish to answer");
         int numberOfQuestion = input.nextInt();
         for (int i = 1; i <= numberOfQuestion; i++) {
-            SecureRandom random = new SecureRandom();
             int firstRandomNumber = random.nextInt(10);
             int secondRandomNumber = random.nextInt(10);
             question(firstRandomNumber, secondRandomNumber);
         }
     }
-    public void question(int firstRandomNumber, int secondRandomNumber) {
-        int answer = (firstRandomNumber * secondRandomNumber);
-            System.out.println("What is the product of " + firstRandomNumber + " And " + secondRandomNumber);
+    public void question(int numberOne, int numberTwo) {
+        int answer = (numberOne * numberTwo);
+        while (studentAnswer != answer) {
+            System.out.println("What is the product of " + numberOne + " And " + numberTwo);
             int studentAnswer = input.nextInt();
-            if (studentAnswer == answer){
+            if (studentAnswer == answer) {
                 System.out.println("Very good");
-            }else {
-                System.out.println("Wrong! Try another");
+                break;
+            } else {
+                System.out.println("Wrong! Try it again");
             }
         }
+   }
 
     public static void main(String[] args) {
         ComputerAssistedInstruction instruction = new ComputerAssistedInstruction();
